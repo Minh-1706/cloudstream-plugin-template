fb75c4cd7db1c78c036741b4827a24220a13450f
Minh-1706 <lamcongminh1706@gmail.com>
2026-03-26 06:23:52 +0700

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,21 +1,26 @@
 package com.example
 
-import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.newMovieSearchResponse
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.HomePageRequest
+import com.lagradost.cloudstream3.HomePageResponse
+import com.lagradost.cloudstream3.newHomePageResponse
 
-class ExampleProvider : MainAPI() { // all providers must be an instance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
-    override val supportedTypes = setOf(TvType.Movie)
+class Yanhh3DProvider : MainAPI() { 
+    override var mainUrl = "https://yanhh3d.sh"
+    override var name = "Yanhh3D"
+    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
 
-    override var lang = "en"
-
-    // enable this when your provider has a main page
-    override val hasMainPage = true
-
-    // this function gets called when you search for something
-    override suspend fun search(query: String): List<SearchResponse> {
-        return listOf<SearchResponse>()
+    override suspend fun getMainPage(page: Int, request: HomePageRequest): HomePageResponse {
+        val document = app.get(mainUrl).document
+        val home = document.select("article, .post-item").mapNotNull {
+            val title = it.selectFirst(".entry-title, h2")?.text() ?: return@mapNotNull null
+            val href = it.selectFirst("a")?.attr("href") ?: return@mapNotNull null
+            val poster = it.selectFirst("img")?.attr("src") ?: ""
+            newMovieSearchResponse(title, href, TvType.Movie) { this.posterUrl = poster }
+        }
+        return newHomePageResponse("Phim Mới", home)
     }
 }
