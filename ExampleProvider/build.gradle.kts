c8620a06c58c0e72e452e785b8754bf7b977b0b5
Minh-1706 <lamcongminh1706@gmail.com>
2026-03-28 01:29:52 +0700

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,24 +1,20 @@
-// use an integer for version numbers
-version = 1
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
 
+android {
+    compileSdk = 34
+    namespace = "com.example"
 
-cloudstream {
-    // All of these properties are optional, you can safely remove them
+    defaultConfig {
+        minSdk = 21
+    }
+}
 
-    description = "Lorem Ipsum"
-    authors = listOf("Cloudburst")
-
-    /**
-    * Status int as the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta only
-    * */
-    status = 1 // will be 3 if unspecified
-
-    // List of video source types. Users are able to filter for extensions in a given category.
-    // You can find a list of avaliable types here:
-    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
-    tvTypes = listOf("Others")
+dependencies {
+    val cloudstreamVersion = "3.0.0" 
+    compileOnly("com.lagradost:cloudstream3:$cloudstreamVersion")
+    implementation("com.github.Blatzar:NiceHttp:0.4.9")
+    implementation("org.jsoup:jsoup:1.16.1")
 }
