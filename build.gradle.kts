ec1bd6349802455597abcab9551eb117ee32ea13
Minh-1706 <lamcongminh1706@gmail.com>
2026-03-28 01:29:15 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
     }
 }
 
@@ -16,23 +14,5 @@ allprojects {
         google()
         mavenCentral()
         maven("https://jitpack.io")
-        maven("https://realsrv.com/repository/public/")
     }
-}
-
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-
-    configurations.all {
-        resolutionStrategy.eachDependency {
-            if (requested.group == "com.github.Blatzar" && requested.name == "NiceHttp") {
-                useVersion("0.4.9")
-            }
-        }
-    }
-}
-
-task<Delete>("clean") {
-    delete(rootProject.buildDir)
 }
